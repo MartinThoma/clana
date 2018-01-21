1ce07e7ad2bf8302ce4acc83c5dd8b7421c058c4
Martin Thoma <info@martin-thoma.de>
2018-01-21 19:30:10 +0100

    MAINT: There shall only be one executable

--- clana/get_cm.py
+++ clana/get_cm.py
@@ -10,6 +10,47 @@ import logging
 
 # 3rd party modules
 import numpy as np
+import click
+
+
+@click.command(name='get-cm', help=__doc__)
+@click.option('--predictions', 'cm_dump_filepath',
+              required=True,
+              type=click.Path(exists=True),
+              help='CSV file with delimiter ;')
+@click.option('--gt', 'gt_filepath',
+              required=True,
+              type=click.Path(exists=True),
+              help='CSV file with delimiter ;')
+@click.option('--n', 'n',
+              required=True,
+              type=int,
+              help='Number of classes')
+def main(cm_dump_filepath, gt_filepath, n):
+    """
+    Calculate a confusion matrix.
+
+    Parameters
+    ----------
+    cm_dump_filepath : str
+        CSV file with delimter ; and quoting char "
+        The first field is an identifier, the second one is the index of the
+        predicted label
+    gt_filepath : str
+        CSV file with delimter ; and quoting char "
+        The first field is an identifier, the second one is the index of the
+        ground truth
+    n : int
+        Number of classes
+    """
+    cm = calculate_cm(cm_dump_filepath, gt_filepath, n)
+    # Write JSON file
+    path = 'cm.json'
+    with open(path, 'w') as outfile:
+        str_ = json.dumps(cm.tolist(), indent=2,
+                          separators=(',', ': '), ensure_ascii=False)
+        outfile.write(str_)
+    logging.info('cm was written to \'{}\''.format(path))
 
 
 def calculate_cm(cm_dump_filepath, gt_filepath, n):
@@ -26,6 +67,8 @@ def calculate_cm(cm_dump_filepath, gt_filepath, n):
         CSV file with delimter ; and quoting char "
         The first field is an identifier, the second one is the index of the
         ground truth
+    n : int
+        Number of classes
 
     Returns
     -------
@@ -46,46 +89,12 @@ def calculate_cm(cm_dump_filepath, gt_filepath, n):
     for identifier, truth_index in truths:
         ident2truth_index[identifier] = int(truth_index)
 
-    assert len(predictions) == len(truths), \
-        "len(predictions) = {} != {} = len(truths)".format(len(predictions),
-                                                           len(truths))
+    if len(predictions) != len(truths):
+        msg = ('len(predictions) = {} != {} = len(truths)"'
+               .format(len(predictions), len(truths)))
+        raise ValueError(msg)
 
     for ident, pred_index in predictions:
         cm[ident2truth_index[ident]][int(pred_index)] += 1
 
     return cm
-
-
-def get_parser():
-    """Get parser object for script xy.py."""
-    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-    parser = ArgumentParser(description=__doc__,
-                            formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-p", "--predictions",
-                        dest="cm_dump_filepath",
-                        help="CSV file with delimiter ;",
-                        required=True,
-                        metavar="FILE")
-    parser.add_argument("-t", "--truth",
-                        dest="gt_filepath",
-                        help="CSV file with delimiter ;",
-                        required=True,
-                        metavar="FILE")
-    parser.add_argument("-n",
-                        dest="n",
-                        required=True,
-                        type=int,
-                        help="Total number of classes")
-    return parser
-
-
-if __name__ == "__main__":
-    args = get_parser().parse_args()
-    cm = calculate_cm(args.cm_dump_filepath, args.gt_filepath, args.n)
-    # Write JSON file
-    path = 'cm.json'
-    with open(path, 'w') as outfile:
-        str_ = json.dumps(cm.tolist(), indent=2,
-                          separators=(',', ': '), ensure_ascii=False)
-        outfile.write(str_)
-    logging.info('cm was written to \'{}\''.format(path))
