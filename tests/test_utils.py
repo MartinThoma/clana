40c0d4a6ac2478586645aa8e70f384f4a0dc30b2
Martin Thoma <info@martin-thoma.de>
2022-02-06 13:49:05 +0100

    Replace click by typer

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,9 @@
+# Core Library
+from pathlib import Path
+
 # First party
 import clana.utils
 
 
 def test_load_labels() -> None:
-    clana.utils.load_labels("~/.clana/data/labels.csv", 10)
+    clana.utils.load_labels(Path("~/.clana/data/labels.csv"), 10)
